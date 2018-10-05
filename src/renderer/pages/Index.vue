5c431a75b49ead2fdb99826611f9595889f16658
Maxwell-Alexius <destinydragon85614@gmail.com>
2018-10-05 16:04:36 +0800

    Prepare for statistic page

--- src/renderer/pages/Index.vue
+++ src/renderer/pages/Index.vue
@@ -1,91 +1,16 @@
 <template>
   <main>
-    <section class="messages">
-      <h2><img :src="icons.feedback" alt="Push Notifications"> Notifications</h2>
-      <ul v-if="messages.length">
-        <li
-          v-for="{ id, type, message, info, createdAt } in messages"
-          :key="id"
-        >
-          <div class="img-wrapper">
-            <img :src="MessageIcons[type]" alt="Icon" class="icon" />
-          </div>
-          <div class="message-info">
-            <p>{{ message }}</p>
-            <p
-              class="additional-info"
-              v-for="({ title, content, type, color }, i) in info"
-              :key="`${i}-${title || 'none'}`"
-            >
-              <span v-if="type === 'info'" class="info-wrapper">
-                <img :src="icons.info" alt="Info icon" />
-                <span v-if="title" class="text-wrapper" v-html="`${title} - ${content}`" />
-                <span v-else class="text-wrapper" v-html="content" />
-              </span>
-
-              <span v-else-if="type === 'tag'" class="info-wrapper">
-                <tag
-                  v-if="title"
-                  :title="title"
-                  :sub-title="content"
-                  :style="{
-                    'background-color': color[0],
-                    'color': color[1],
-                  }"        
-                />
-                <tag
-                  v-else
-                  :title="content"
-                  :style="{
-                    'background-color': color[0],
-                    'color': color[1],
-                  }"        
-                />
-              </span>
-            </p>
-            <p class="additional-info">
-              <span class="info-wrapper">
-                <img :src="icons.clock" alt="Notification Created At" />
-                <span class="text-wrapper">{{ timestampStringify(createdAt) }}</span>
-              </span>
-            </p>
-          </div>
-        </li>
-      </ul>
-
-      <p v-else class="no-message">
-        Currently there are no message ...
-      </p>
-    </section>
+    <notifications class="notifications" />
+    <statistics class="statistics" />
   </main>
 </template>
 
 <script>
-import Tag from '@/components/Tag';
-import { MessageIcons } from '@/resources/message-types';
-import clock from '@/assets/clock.svg';
-import info from '@/assets/info.svg';
-import feedback from '@/assets/feedback.svg';
-import { timestampStringify } from '@/helpers/time';
+import Notifications from '@/pages/Index/Notifications';
+import Statistics from '@/pages/Index/Statistics';
 
 export default {
-  components: { Tag },
-  data() {
-    return {
-      MessageIcons,
-      icons: {
-        clock,
-        info,
-        feedback,
-      },
-    };
-  },
-  computed: {
-    messages() { return this.$store.getters['messages/data']; },
-  },
-  methods: {
-    timestampStringify,
-  },
+  components: { Notifications, Statistics },
 };
 </script>
 
@@ -95,98 +20,5 @@ export default {
 main
   width: 960px
   margin: 0 auto
-  padding: 
-  > section
-    > h2
-      @include vertical-align
-      line-height: 60pt
-      padding-left: 15pt
-      font-family: 'Roboto Mono', sans-serif
-      font-weight: 100
-      > img
-        opacity: 0.5
-
-  > section.messages
-    margin-top: -50pt
-    height: 100vh
-    width: 50%
-    padding: 50pt 15pt 0 15pt
-    box-sizing: border-box
-    background-color: #444
-    > p.no-message
-      font-size: 12pt
-      padding-left: 15pt
-      color: rgba(255, 255, 255, 0.32)
-    > ul
-      height: calc(100vh - 110pt)
-      overflow-y: auto
-      box-sizing: border-box
-      padding-bottom: 100pt
-      > li
-        box-sizing: border-box
-        padding: 0 5pt
-        font-size: 0
-        border-radius: 5pt
-        position: relative
-        &:before
-          content: ''
-          display: inline-block
-          position: absolute
-          width: 2pt
-          height: calc(100% - 10pt)
-          background-color: #666
-          left: 14pt
-          top: 30pt
-        + li
-          margin-top: 20pt
-        &:last-child:after
-          content: ''
-          display: inline-block
-          width: 10pt
-          height: 10pt
-          border-radius: 10pt
-          background-color: #666
-          position: absolute
-          left: 10pt
-          bottom: -25pt
-
-        > div.img-wrapper
-          position: absolute
-          width: 30pt
-          height: 30pt
-          left: 0
-          top: 0
-          text-align: center
-          border-radius: 15pt
-          border: 2pt solid #666
-          box-sizing: border-box
-          @include vertical-align
-          > img.icon
-            width: 16pt
-            height: 16pt
-
-        > div.message-info
-          padding: 6pt 0 5pt 30pt
-          > p
-            font-size: 12pt
-            line-height: 1.5
-            &:first-child
-              margin-bottom: 5pt
-          > p.additional-info
-            height: 20pt
-            + p.additional-info
-              margin-top: 1pt
-            > span.info-wrapper
-              display: inline-block
-              height: 20pt
-              @include vertical-align
-              > span.text-wrapper
-                font-size: 9pt
-                color: rgba(255, 255, 255, 0.54)
-
-              > img
-                width: 12pt
-                height: 12pt
-                opacity: 0.4
-                margin-right: 3pt
+  font-size: 0
 </style>
---
+++ src/renderer/pages/Index/Statistics.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h2><img :src="icons.barChart" alt="Statistics"> Statistics</h2>
+  </section>
+</template>
+
+<script>
+import barChart from '@/assets/bar-chart.svg';
+
+export default {
+  data() {
+    return {
+      icons: { barChart },
+    };
+  },
+};
+</script>
+
+<style scoped lang="sass">
+@import '../../sass/_shared.sass'
+
+section
+  margin-top: -50pt
+  height: 100vh
+  width: calc(50% - 10pt)
+  margin-left: 20pt
+  padding: 50pt 15pt 0 15pt
+  box-sizing: border-box
+  background-color: #444
+  display: inline-block
+  vertical-align: top
+  > h2
+    @include vertical-align
+    font-size: 16pt
+    line-height: 60pt
+    padding-left: 15pt
+    font-family: 'Roboto Mono', sans-serif
+    font-weight: 100
+    > img
+      opacity: 0.5
+</style>
